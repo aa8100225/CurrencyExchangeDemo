85e812f0bd1bd33992ea28c8d55e1cf8db0d6341
Ian <ian.dai.wk@gmail.com>
2024-05-01 12:56:31 +0800

    Enhance Service layer: Add validation and parsing for amount input

--- src/main/java/main/CurrencyExchangeService.java
+++ src/main/java/main/CurrencyExchangeService.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ParseException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +17,33 @@ public class CurrencyExchangeService {
 		this.currencyConversionRateConfig = currencyConversionRateConfig;
 	}
 
-	public String convert(String source, String target, String amount) {
+	public String convert(String source, String target, String amount) throws Exception {
+		BigDecimal decimalAmount;
+		try {
+			decimalAmount = parseStringToDecimal(amount);
+		} catch (Exception e) {
+			throw new Exception("Amount has invalid format.");
+		}
 
 		return "0.00";
+	}
+
+	public BigDecimal parseStringToDecimal(String amount) throws Exception {
+		// All or none: if formatted, the entire string must be formatted correctly
+		if (amount == null || !amount.matches("^[+-]?(\\d+|(\\d{1,3}(,\\d{3})+))(\\.\\d+)?$")) {
+			throw new Exception("Invalid number format");
+		}
+		amount = amount.replace(",", "");
+		DecimalFormat decimalFormat = new DecimalFormat();
+		decimalFormat.setParseBigDecimal(true);
+
+		BigDecimal decimal;
+		try {
+			decimal = new BigDecimal(decimalFormat.parse(amount).toString());
+		} catch (ParseException e) {
+			throw e;
+		}
+		return decimal;
 	}
 
 }
